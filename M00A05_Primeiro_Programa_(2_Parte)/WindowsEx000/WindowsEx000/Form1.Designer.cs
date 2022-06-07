c91a0f3f19bacfb1054357e4b7a471eff1c0b1fe
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-07 19:07:10 +0100

    M00A05 – Primeiro Programa em C# (2ª Parte)

--- M00A05_Primeiro_Programa_(2_Parte)/WindowsEx000/WindowsEx000/Form1.Designer.cs
+++ M00A05_Primeiro_Programa_(2_Parte)/WindowsEx000/WindowsEx000/Form1.Designer.cs
@@ -28,14 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(104, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 70);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.MaximizeBox = false;
+            this.ClientSize = new System.Drawing.Size(459, 280);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Primeiro Programa C#";
             this.TopMost = true;
             this.ResumeLayout(false);
@@ -43,6 +64,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
