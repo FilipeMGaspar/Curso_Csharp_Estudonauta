c302607471edee283d5e76d839ed56f3fd06f663
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-13 10:00:15 +0100

    M01A02 – Mostrando coisas na tela com C#

--- Modulo_01_Estruturas_Basicas/Modulo_01_Estrut_Basicas/M01A02_D/Program.cs
+++ Modulo_01_Estruturas_Basicas/Modulo_01_Estrut_Basicas/M01A02_D/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("C# é \n \"SUPER\"\nFácil!");
+Console.WriteLine("C# é \n\"SUPER\"\nFácil!\a");
+Console.ReadKey();
