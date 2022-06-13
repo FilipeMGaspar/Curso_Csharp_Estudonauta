3c180abf9b5380d0114cfeb8950905bab7ca9cc8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-13 10:12:27 +0100

    M01A02 – Mostrando coisas na tela com C#

--- Modulo_01_Estruturas_Basicas/Modulo_01_Estrut_Basicas/M01A02_E/Program.cs
+++ Modulo_01_Estruturas_Basicas/Modulo_01_Estrut_Basicas/M01A02_E/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.SetCursorPosition(20, 5);
+Console.SetCursorPosition(20, 5); //Recoloca a posição do cursor
+Console.ForegroundColor = ConsoleColor.DarkRed;  //Cor da letra
+Console.BackgroundColor = ConsoleColor.Yellow; //Cor do fundo
 Console.WriteLine("Olá, querido(a) Estudonauta!");
 Console.ReadKey();
+Console.ResetColor(); // Limpa as configurações das cores
 Console.Clear(); // Limpa a tela do console
-Console.SetCursorPosition(10, 15);
+Console.SetCursorPosition(10, 15); //Recoloca a posição do cursor
 Console.WriteLine("Tudo bem com você");
 Console.ReadKey();
