bc242424a29ed6a9e73271790291ef9aa6340015
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-13 10:07:15 +0100

    M01A02 – Mostrando coisas na tela com C#

--- Modulo_01_Estruturas_Basicas/Modulo_01_Estrut_Basicas/M01A02_E/Program.cs
+++ Modulo_01_Estruturas_Basicas/Modulo_01_Estrut_Basicas/M01A02_E/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.SetCursorPosition(20, 0);
 Console.WriteLine("Olá, querido(a) Estudonauta!");
 Console.ReadKey();
 Console.Clear(); // Limpa a tela do console
