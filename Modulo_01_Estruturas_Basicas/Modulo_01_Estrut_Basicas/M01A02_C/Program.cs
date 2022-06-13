dab60a3528ccf4c21602eb324aafa9a7d007a770
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-13 09:50:25 +0100

    M01A02 – Mostrando coisas na tela com C#

---
+++ Modulo_01_Estruturas_Basicas/Modulo_01_Estrut_Basicas/M01A02_C/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Estudonauta!");
+Console.ReadKey();
+
+/*
+ 
+ *  \n    Nova linha
+ *   \t    Tabulação
+ *  \b    Backspace apaga um espaço
+ *  \a    Alerta
+ *  \r    Retorno   Volta ao inicio da linha
+ *  \\    Barra
+ *  \"    Aspas
+ 
+ */
