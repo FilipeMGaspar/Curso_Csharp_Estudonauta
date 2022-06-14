214fadbbde5d0f379fe36c8cb2b1ff648bfca822
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-14 12:54:41 +0100

    M01A03 – Como fazer comentários

--- Modulo_01_Estruturas_Basicas/M01A03_Como_fazer_comentarios/M01A03/M01A03/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A03_Como_fazer_comentarios/M01A03/M01A03/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.ForegroundColor = ConsoleColor.DarkCyan;
-Console.BackgroundColor = ConsoleColor.White;
+Console.SetCursorPosition(20, 10); // Modifica a posição do cursor linha , coluna
+Console.ForegroundColor = ConsoleColor.DarkCyan; // Cor da letra
+Console.BackgroundColor = ConsoleColor.White; // cor de fundo
 Console.WriteLine("Olá, Mundo!");
 Console.ReadKey();
 Console.ResetColor();
