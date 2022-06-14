637edeba954d179a205c9a5cf97d9fd6b4e768c5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-14 13:40:20 +0100

    M01A04 – Tipos Primitivos do C#

--- Modulo_01_Estruturas_Basicas/M01A04_Tipos_Primitivos_do_C_Sharp/M01A04/M01A04/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A04_Tipos_Primitivos_do_C_Sharp/M01A04/M01A04/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("O tipo byte vai de " + byte.MinValue + " até " + byte.MaxValue);
