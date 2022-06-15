138c566273d31c0401af1e4fd605640f87113c29
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-15 14:33:40 +0100

    M01A05 – Usando Variáveis com C#

--- Modulo_01_Estruturas_Basicas/M01A05_Usando_Variaveis_com_C_Sharp/M01A05/M01A05/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A05_Usando_Variaveis_com_C_Sharp/M01A05/M01A05/Program.cs
@@ -1,6 +1,9 @@
 ﻿// Declaração de variaveis
 sbyte n = 20;
 n = -50;
+var nome = "Filipe";
 Console.WriteLine("A variavel n tem o valor de: " + n);
 Console.WriteLine("n é do tipo: " + n.GetType());
+Console.WriteLine("Nome: " + nome);
+Console.WriteLine(nome.GetType());
 Console.ReadKey();
