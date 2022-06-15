fd167d04eab4fcf5eebdab41ed934878ba8a2ccd
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-15 14:41:45 +0100

    M01A05 – Usando Variáveis com C#

--- Modulo_01_Estruturas_Basicas/M01A05_Usando_Variaveis_com_C_Sharp/M01A05/M01A05B/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A05_Usando_Variaveis_com_C_Sharp/M01A05/M01A05B/Program.cs
@@ -3,6 +3,8 @@ ushort publicoEstadio = 24050;
 float media = 4.5f;
 double pib = 25689725.5698;
 decimal estrela = 4.89756982m;
+bool aprovado = false;
 
 Console.WriteLine(estrela);
+Console.WriteLine(aprovado.GetType());
 Console.ReadKey();
