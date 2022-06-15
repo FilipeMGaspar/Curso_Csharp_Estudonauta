70948302a3584791967a38bb357d26854528ed6c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-15 14:51:09 +0100

    M01A05 – Usando Variáveis com C#

--- Modulo_01_Estruturas_Basicas/M01A05_Usando_Variaveis_com_C_Sharp/M01A05/M01A05B/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A05_Usando_Variaveis_com_C_Sharp/M01A05/M01A05B/Program.cs
@@ -7,8 +7,25 @@ bool aprovado = false;
 int x;
 var y = 5.5;
 
+int salarioBruto;
+float nota;
+
+
 Console.WriteLine(estrela);
 Console.WriteLine(aprovado.GetType());
 Console.WriteLine("O valor de y = " + y);
 Console.WriteLine(y.GetType());
 Console.ReadKey();
+
+/*
+ * Regras de C#
+ * 
+ * Começa com uma letra, sublinhado ou @
+ * Maiúsculas e minúsculas fazem diferença
+ * só usa letras, números e sublinhado
+ * Podemos usar acentos
+ * Não pode conter espaços
+ * Não pode ter símbolos (só o sublinhado e a @)
+ * Não pode ser uma palavra reservada
+ * 
+ */
