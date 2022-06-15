a60a7ae37bd66a2f19e626bb730f39bf445f6d2a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-15 13:01:52 +0100

    M01A05 – Usando Variáveis com C#

--- Modulo_01_Estruturas_Basicas/M01A05_Usando_Variaveis_com_C_Sharp/M01A05/M01A05/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A05_Usando_Variaveis_com_C_Sharp/M01A05/M01A05/Program.cs
@@ -1,4 +1,4 @@
-﻿var n = 0;
-Console.WriteLine("A variavel n tem o valor de " + n);
+﻿byte n = 0;
+Console.WriteLine("A variavel n tem o valor de: " + n);
 Console.WriteLine("N é do tipo: " + n.GetType());
 Console.ReadKey();
