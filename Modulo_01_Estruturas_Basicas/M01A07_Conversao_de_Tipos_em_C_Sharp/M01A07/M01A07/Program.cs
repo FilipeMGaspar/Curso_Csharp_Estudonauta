e3e3e3c80eb6f9ed1aeb6b04a78554f1a2bc2972
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-17 11:14:34 +0100

    M01A07 – Conversão de Tipos em C#

--- Modulo_01_Estruturas_Basicas/M01A07_Conversao_de_Tipos_em_C_Sharp/M01A07/M01A07/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A07_Conversao_de_Tipos_em_C_Sharp/M01A07/M01A07/Program.cs
@@ -7,14 +7,20 @@ Console.WriteLine("A = " + a + " do tipo " + a.GetType());
 Console.WriteLine("B = " + b + " do tipo " + b.GetType());
 */
 
-// Conversão Explicita float->int
+// Conversão Explicita por cast coerção float->int
+/*
 float a = 7.75f;
 int b = (int)a; 
 
 Console.WriteLine("A = " + a + " do tipo " + a.GetType());
+Console.WriteLine("B = " + b + " do tipo " + b.GetType());*/
+
+// conversão por classes auxiliares
+float a = 7.99999999f;
+int b = Convert.ToInt16(a);
+
+Console.WriteLine("A = " + a + " do tipo " + a.GetType());
 Console.WriteLine("B = " + b + " do tipo " + b.GetType());
 
-
-// conversão
 
 Console.ReadKey();
