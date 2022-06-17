1a839c1573ca5c83b1eedd2bbdafa075f4e82f36
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-17 10:54:36 +0100

    M01A06 – Constantes em C#

--- Modulo_01_Estruturas_Basicas/M01A05_Usando_Variaveis_com_C_Sharp/M01A06/M01A06/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A05_Usando_Variaveis_com_C_Sharp/M01A06/M01A06/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Estudo no " + ESCOLA);
 Console.WriteLine("Contante num = " + num);
 Console.WriteLine("Variavel n = " + n);
 n = 21;
-Console.WriteLine("Variavel n = " + n);
+Console.WriteLine("Variavel n alterada para " + n);
 // ESCOLA = "Curso Em video"; // Comando dá erro pois uma constante não pode ser modificado o seu valor
 // Console.WriteLine("Agora estudo no " + ESCOLA);
 
