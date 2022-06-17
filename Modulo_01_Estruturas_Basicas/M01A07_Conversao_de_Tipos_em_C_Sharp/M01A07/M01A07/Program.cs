d22b634988609e651ce709b517f673a9d10df4e7
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-17 11:10:52 +0100

    M01A05 – Usando Variáveis com C#

--- Modulo_01_Estruturas_Basicas/M01A07_Conversao_de_Tipos_em_C_Sharp/M01A07/M01A07/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A07_Conversao_de_Tipos_em_C_Sharp/M01A07/M01A07/Program.cs
@@ -1,15 +1,15 @@
 ﻿// Conversão implicita int->float
-
+/*
 int a = 7;
 float b = a;
 
 Console.WriteLine("A = " + a + " do tipo " + a.GetType());
 Console.WriteLine("B = " + b + " do tipo " + b.GetType());
-
+*/
 
 // Conversão Explicita float->int
 float c = 7;
-int d = (int)a; 
+int d = (int)c; 
 
 Console.WriteLine("A = " + c + " do tipo " + c.GetType());
 Console.WriteLine("B = " + d + " do tipo " + d.GetType());
