0b1ea3cf5019189a92265896ac27e5596af53873
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-17 11:06:41 +0100

    M01A05 – Usando Variáveis com C#

--- Modulo_01_Estruturas_Basicas/M01A07_Conversao_de_Tipos_em_C_Sharp/M01A07/M01A07/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A07_Conversao_de_Tipos_em_C_Sharp/M01A07/M01A07/Program.cs
@@ -2,7 +2,7 @@
 int a = 7;
 float b = a;
 
-Console.WriteLine("A = " + a);
-Console.WriteLine("B = " + b);
+Console.WriteLine("A = " + a + " do tipo " + a.GetType());
+Console.WriteLine("B = " + b + " do tipo " + b.GetType());
 
 Console.ReadKey();
