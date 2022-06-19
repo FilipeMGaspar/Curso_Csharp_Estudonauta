ea2b306373277a9ee00924c6c0c9c0f1df6b469f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-19 10:40:09 +0100

    M01A08 – Entrada de Dados com C#

--- Modulo_01_Estruturas_Basicas/M01A08_Entrada_de_Dados_com_C_Sharp/M01A08/M01A08Int/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A08_Entrada_de_Dados_com_C_Sharp/M01A08/M01A08Int/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿/* Lendo numeros */
+int num, d;
+
+Console.Write("Insira um número: ");
+num = Convert.ToInt32(Console.ReadLine());
+d = num * 2;
+Console.Write("O dobro de " + num + " é igual a  " + d);
+Console.ReadKey();
