a63c163646c4eab4b31d15e3b6bcafeef87437d9
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-19 10:59:22 +0100

    M01A08 – Entrada de Dados com C#

--- Modulo_01_Estruturas_Basicas/M01A08_Entrada_de_Dados_com_C_Sharp/M01A08/M01A08IntTentaConv/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A08_Entrada_de_Dados_com_C_Sharp/M01A08/M01A08IntTentaConv/Program.cs
@@ -1,3 +1,9 @@
 ﻿/* Tenta/testa conversão */
 int num = 0;
+int d;
+
+Console.Write("Digite um número: ");
 int.TryParse(Console.ReadLine(), out num);
+d = num * 2;
+Console.WriteLine("O dobro de " + num + " é " + d);
+Console.ReadKey();
