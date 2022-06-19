4b79b2f563a9608cdcfb6ce54b9971a7f0d0e745
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-19 10:15:24 +0100

    M01A08 – Entrada de Dados com C#

--- Modulo_01_Estruturas_Basicas/M01A08_Entrada_de_Dados_com_C_Sharp/M01A08/M01A08/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A08_Entrada_de_Dados_com_C_Sharp/M01A08/M01A08/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Qual é o seu nome: ");
 string nome = Console.ReadLine();
-Console.WriteLine("Olá " + nome + " ,seja bem vindo");
+Console.WriteLine("Olá " + nome + ", seja bem vindo");
 
 Console.ReadKey();
