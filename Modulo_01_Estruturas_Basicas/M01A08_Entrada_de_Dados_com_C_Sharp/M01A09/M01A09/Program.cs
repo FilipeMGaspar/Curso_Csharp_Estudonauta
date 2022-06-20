0826366f0c6a5ccb12aee31a9dea8d620478d20e
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-20 19:14:39 +0100

    M01A09 – Números Aleatórios em C#

--- Modulo_01_Estruturas_Basicas/M01A08_Entrada_de_Dados_com_C_Sharp/M01A09/M01A09/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A08_Entrada_de_Dados_com_C_Sharp/M01A09/M01A09/Program.cs
@@ -1,4 +1,5 @@
 ﻿Random gerador = new Random();
-int n = gerador.Next(100);
+int n = gerador.Next(5);  // de 0 até 4
+int m = gerador.Next(1, 4); // gera numeros de 1 até 3
 Console.WriteLine("Acabei de gerar o número: " + n);
 Console.ReadKey();
