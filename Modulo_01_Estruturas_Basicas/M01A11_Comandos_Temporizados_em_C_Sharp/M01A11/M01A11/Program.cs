7e7dde52ca1c070836f091bebf15a7af11a1b655
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-24 08:15:11 +0100

    M01A11- Comandos Temporizados em C#

--- Modulo_01_Estruturas_Basicas/M01A11_Comandos_Temporizados_em_C_Sharp/M01A11/M01A11/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A11_Comandos_Temporizados_em_C_Sharp/M01A11/M01A11/Program.cs
@@ -1,3 +1,17 @@
-﻿Thread.Sleep(2000); // tempo em milisegundos
+﻿int dia = DateTime.Now.Day;
+int mes = DateTime.Now.Month;
+int ano = DateTime.Now.Year;
+
+Console.Write("Hoje ");
+Thread.Sleep(1000); // tempo em milisegundos
+Console.Write(dia);
+Thread.Sleep(1000);
+Console.Write(" do mês ");
+Thread.Sleep(1000);
+Console.WriteLine(mes);
+Thread.Sleep(1000);
+Console.WriteLine(" do ano de ");
+Thread.Sleep(1000);
+Console.Write(ano);
 
 Console.ReadKey();
