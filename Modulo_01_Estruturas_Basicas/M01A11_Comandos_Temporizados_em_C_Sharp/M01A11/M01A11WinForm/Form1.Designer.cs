d071a3984127d746edb54c4766cb7edbba76b9ed
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-24 08:20:57 +0100

    M01A11- Comandos Temporizados em C#

--- Modulo_01_Estruturas_Basicas/M01A11_Comandos_Temporizados_em_C_Sharp/M01A11/M01A11WinForm/Form1.Designer.cs
+++ Modulo_01_Estruturas_Basicas/M01A11_Comandos_Temporizados_em_C_Sharp/M01A11/M01A11WinForm/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnOk.Location = new System.Drawing.Point(63, 97);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(168, 50);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(63, 39);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(66, 15);
+            this.lblMsg.TabIndex = 1;
+            this.lblMsg.Text = "Mensagem";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(360, 209);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.btnOk);
+            this.Name = "Form1";
+            this.Text = "M01A11- Comandos Temporizados em C#";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnOk;
+        private Label lblMsg;
     }
 }
