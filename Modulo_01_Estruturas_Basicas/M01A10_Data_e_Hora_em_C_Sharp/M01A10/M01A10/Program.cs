ad8fa7cf97b27751270f7dfc167b4c629f03f37c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-24 08:06:38 +0100

    M01A10 – Data e Hora em C#

--- Modulo_01_Estruturas_Basicas/M01A10_Data_e_Hora_em_C_Sharp/M01A10/M01A10/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A10_Data_e_Hora_em_C_Sharp/M01A10/M01A10/Program.cs
@@ -1,12 +1,12 @@
 ﻿int dia = DateTime.Now.Day;
 int mes = DateTime.Now.Month;
 int ano = DateTime.Now.Year;
-Console.SetCursorPosition(10 , 5);
+Console.SetCursorPosition(10 , 5); // coluna, linha
 Console.WriteLine(dia + "/" + mes + "/" + ano);
 
 int hora = DateTime.Now.Hour;
 int minuto = DateTime.Now.Minute;
 int segundo = DateTime.Now.Second;
-Console.SetCursorPosition (10 , 6);
+Console.SetCursorPosition (10 , 6); // coluna, linha
 Console.WriteLine(hora + ":"+ minuto + ":" + segundo);
 Console.ReadKey();
