c57f37874472ccdd518107f4d62d8ba8a143e58f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-26 17:49:48 +0100

    M01A12 – Strings Interpoladas em C#

--- Modulo_01_Estruturas_Basicas/M01A12_Strings_Interpoladas_em_C_Sharp/M01A12/M01A12/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A12_Strings_Interpoladas_em_C_Sharp/M01A12/M01A12/Program.cs
@@ -2,5 +2,10 @@
 Console.Write("Qual é o seu nome? ");
 nome = Console.ReadLine();
 //Console.WriteLine("Olá " + nome + "! Tudo bem?" );
-Console.WriteLine($"Olá {nome}, tudo bem?");
+Console.WriteLine($"Olá {nome}! Tudo bem?");
+
+int dia = DateTime.Now.Day;
+int mes = DateTime.Now.Month;
+int ano = DateTime.Now.Year;
+
 Console.ReadKey();
