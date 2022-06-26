9141dbe29a874bcf92d36cce8717e8492b6452ba
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-26 18:05:40 +0100

    M01A12 – Strings Interpoladas em C#

--- Modulo_01_Estruturas_Basicas/M01A12_Strings_Interpoladas_em_C_Sharp/M01A12/M01A12_B/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A12_Strings_Interpoladas_em_C_Sharp/M01A12/M01A12_B/Program.cs
@@ -1,5 +1,5 @@
 ﻿float sal = 0;
 Console.Write("Qual é o seu salário? ");
 float.TryParse(Console.ReadLine(), out sal);
-Console.WriteLine($"Você ganha {sal}€ por mês");
+Console.WriteLine($"Você ganha {sal:C} por mês");
 Console.ReadKey();
