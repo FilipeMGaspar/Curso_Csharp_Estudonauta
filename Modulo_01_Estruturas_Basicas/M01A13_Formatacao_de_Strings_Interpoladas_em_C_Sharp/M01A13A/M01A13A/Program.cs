5364dd82fd66db2f50a4a29c2fe8b575825bcd70
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-27 10:21:28 +0100

    M01A13 – Formatação de Strings Interpoladas em C#

--- Modulo_01_Estruturas_Basicas/M01A13_Formatacao_de_Strings_Interpoladas_em_C_Sharp/M01A13A/M01A13A/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A13_Formatacao_de_Strings_Interpoladas_em_C_Sharp/M01A13A/M01A13A/Program.cs
@@ -1,8 +1,22 @@
 ﻿string nome = "Filipe";
 float sal = 550.7556f;
+int idade = 22;
 
-Console.WriteLine($"O {nome, -10} ganha {sal, 10:c3} por mês");
+Console.WriteLine($"O {nome, -10} tem {idade} anos e ganha {sal, 10:c3} por mês");
 
 //Console.WriteLine($"Muito prazer {nome, -3}");
 
 Console.ReadKey();
+
+/*  Formatação numérica
+ *  
+    :C  Moeda (currency)
+    
+    :D  Decimal (inteiro)
+    
+    :N  Número (real)
+
+    :E  Científico
+
+    :X  Hexadecimal
+ */
