6a94fbd9132dd91681dbf6d7170d4353a5f7a934
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-28 09:39:53 +0100

    M01A14 - Exercício 002

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX002/Form1.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX002/Form1.cs
@@ -15,9 +15,18 @@ namespace M01A14_EX002
             lblBr.Visible = true;
             await Task.Delay(300);
             lblBrleiro.Visible = true;
+
             await Task.Delay(1000);
             lblMeu.BackColor = Color.Blue;
             lblMeu.ForeColor = Color.White;
+
+            await Task.Delay(1000);
+            lblBr.BackColor = Color.Yellow;
+            lblBr.ForeColor = Color.DarkGreen;
+
+            await Task.Delay(1000);
+            lblBrleiro.BackColor = Color.DarkGreen;
+            lblBrleiro.ForeColor = Color.Yellow;
         }
     }
 }
