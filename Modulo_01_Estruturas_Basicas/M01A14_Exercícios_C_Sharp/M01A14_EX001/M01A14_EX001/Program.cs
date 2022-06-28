1df1614e53a1ff79bad71b5ec6ccf9f0c8ec59ec
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-28 08:28:54 +0100

    M01A14_Exercício_001

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_EX001/M01A14_EX001/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_EX001/M01A14_EX001/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Thread.Sleep(1000);
+Console.Write("Meu");
+Console.Write("Brasil");
+Console.WriteLine("Brasileiro");
+
+Console.ReadKey();
