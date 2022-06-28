70fd7152ee7d454677d94fbf29ebfa4914a9688a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-28 08:32:33 +0100

    M01A14_Exercício_001

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_EX001/M01A14_EX001/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_EX001/M01A14_EX001/Program.cs
@@ -1,5 +1,10 @@
-﻿Thread.Sleep(1000);
+﻿Console.SetCursorPosition(10,5);
+Thread.Sleep(1000);
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.White;
 Console.Write("Meu");
+Console.BackgroundColor= ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Green;
 Thread.Sleep(1000);
 Console.Write(" Brasil");
 Thread.Sleep(1000);
