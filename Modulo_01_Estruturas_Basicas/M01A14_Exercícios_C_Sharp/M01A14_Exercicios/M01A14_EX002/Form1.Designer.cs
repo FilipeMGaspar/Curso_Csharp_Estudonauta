edfa0fa105dc23c9753eade48b8791a46f8eb22e
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-28 09:25:15 +0100

    M01A14 - Exercício 002

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX002/Form1.Designer.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX002/Form1.Designer.cs
@@ -48,27 +48,27 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(74, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(31, 15);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Meu";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(126, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.Size = new System.Drawing.Size(35, 15);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "Brasil";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(190, 48);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.Size = new System.Drawing.Size(55, 15);
             this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
+            this.label3.Text = "Brasileiro";
             // 
             // Form1
             // 
