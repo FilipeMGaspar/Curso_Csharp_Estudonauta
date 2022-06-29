d985d3aaf7fb0673f68f23c7f412463bc9bce789
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-29 09:48:41 +0100

    M01A14 - Exercício 003

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_Ex003/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_Ex003/Program.cs
@@ -3,4 +3,5 @@ Console.Write("Digite um número real: ");
 float.TryParse(Console.ReadLine(), out num);
 
 Console.WriteLine("----------------------------");
+Console.WriteLine($"Você digitou o valor {num}");
 Console.ReadKey();
