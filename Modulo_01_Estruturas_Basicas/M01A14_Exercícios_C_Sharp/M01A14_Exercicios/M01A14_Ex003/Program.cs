b47e0cdb9e8fc4ad3cab173c454c05438b9ffea4
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-29 10:00:05 +0100

    M01A14 - Exercício 003

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_Ex003/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_Ex003/Program.cs
@@ -2,7 +2,7 @@
 Console.Write("Digite um número real: ");
 float.TryParse(Console.ReadLine(), out num);
 
-Console.WriteLine("-------------------------------");
+Console.WriteLine("--------------------------------");
 Console.WriteLine($"Você digitou o valor {num}");
 Console.WriteLine($"A parte inteira do número é {(int)num}");
 Console.WriteLine($"Arredondando, temos o número {Convert.ToInt16(num)}");
