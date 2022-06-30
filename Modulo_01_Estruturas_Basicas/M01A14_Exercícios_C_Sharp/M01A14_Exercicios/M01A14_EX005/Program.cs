88f0af849e11cc200fb451516d15cbb6bc4698e8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-06-30 14:12:33 +0100

    M01A14 - Exercício 005

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX005/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX005/Program.cs
@@ -1,5 +1,8 @@
 ﻿int anoNasc;
+int anoAtual = DateTime.Now.Year;
 Console.Write("Em que ano você nasceu? ");
 int.TryParse(Console.ReadLine(), out anoNasc);
 Console.WriteLine("---------------------------------------");
+Console.WriteLine($"Estamos no ano de {anoAtual}");
+Console.WriteLine($"Quem nasceu em {anoNasc}, tem {anoAtual - anoNasc}");
 Console.ReadKey();
