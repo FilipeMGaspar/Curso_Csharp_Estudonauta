d6e4b396641b33cdf6546aaeb1f2c153505c9962
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-01 14:35:27 +0100

    M01A14 - WinForm- Exercício 006

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_WinForm_EX006/Form1.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_WinForm_EX006/Form1.cs
@@ -16,7 +16,8 @@ namespace M01A14_WinForm_EX006
             lblAnoAtual.Visible = true;
             lblAnoAtual.Text += anoAtual;
 
-            lbl_Idade.Text = $"Quem nasceu {anoNasc}, vai ter {anoAtual - anoNasc}";
+            await Task.Delay(500);
+            lbl_Idade.Text = $"Quem nasceu {anoNasc}, vai ter {anoAtual - anoNasc} anos";
             lbl_Idade.Visible = true;
         }
     }
