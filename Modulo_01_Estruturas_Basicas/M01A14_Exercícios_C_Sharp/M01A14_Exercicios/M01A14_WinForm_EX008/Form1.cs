23e45dfb8d22fabe8dd1e96065842033b611e447
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-04 20:15:58 +0100

    M01A17 - Exercício 008

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_WinForm_EX008/Form1.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_WinForm_EX008/Form1.cs
@@ -9,7 +9,10 @@ namespace M01A14_WinForm_EX008
 
         private void Btn_sorteia_Click(object sender, EventArgs e)
         {
+            int inicio;
+            int fim;
 
+            int.TryParse(txt_Inicio.Text, out inicio);
         }
     }
 }
