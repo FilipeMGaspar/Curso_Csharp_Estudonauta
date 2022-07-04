4967203339e12b5681eb04aa6a319d0b40ad6373
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-04 20:04:08 +0100

    M01A17 - Exercício 007

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX007/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX007/Program.cs
@@ -1,7 +1,7 @@
 ﻿int inicio;
 int fim;
 
-//gerador = new Random;
+Random gerador = new Random();
 
 Console.WriteLine("SORTEADOR DE NÚMERO");
 Console.WriteLine("-------------------");
@@ -9,6 +9,8 @@ Console.Write("Início: ");
 int.TryParse(Console.ReadLine(), out inicio);
 Console.Write("Fim: ");
 int.TryParse(Console.ReadLine(), out fim);
+
+int sorte = gerador.Next(inicio, (fim +1));
 Console.WriteLine("-------------------");
 Thread.Sleep(1000);
 Console.Write("Vou sertear um número ");
@@ -22,6 +24,7 @@ Thread.Sleep(350);
 Console.Write(".");
 Thread.Sleep(350);
 Console.Write(".\n");
-Thread.Sleep(600);
-Console.WriteLine($"Entre {inicio} e {fim} sorteei o número ");
+Thread.Sleep(700);
+Console.WriteLine();
+Console.WriteLine($"Entre {inicio} e {fim} sorteei o número {sorte}");
 Console.ReadKey();
