655d87245bcb88c8e9eee9f8c3c66e133cb1d5a3
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-04 19:52:25 +0100

    M01A17 - Exercício 007

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX007/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX007/Program.cs
@@ -8,5 +8,15 @@ int.TryParse(Console.ReadLine(), out inicio);
 Console.Write("Fim: ");
 int.TryParse(Console.ReadLine(), out fim);
 Console.WriteLine("-------------------");
+Thread.Sleep(1000);
+Console.WriteLine("Vou sertear um número ");
+Thread.Sleep(200);
+Console.Write(".");
+Thread.Sleep(200);
+Console.Write(".");
+Thread.Sleep(200);
+Console.Write(".");
+Thread.Sleep(200);
+Console.Write(".");
 
 Console.ReadKey();
