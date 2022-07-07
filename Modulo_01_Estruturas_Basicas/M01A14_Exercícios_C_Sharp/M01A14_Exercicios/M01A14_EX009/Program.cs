97c48bfa420f43676441edc32a71e846031a4085
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-07 08:07:29 +0100

    M01A14 - Exercício 009

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX009/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX009/Program.cs
@@ -1,11 +1,17 @@
-﻿string nomeProd;
-float preco;
-Console.WriteLine("Produto");
+﻿string nomeProd, nomeProd2;
+float preco, precoProd2;
+Console.WriteLine("Produto 1");
 Console.Write("\tNome ..: ");
 nomeProd = Console.ReadLine();
 Console.Write("\tPreço ..: ");
 float.TryParse(Console.ReadLine(), out preco);
 
-Console.WriteLine($"{nomeProd} {preco, 10:C2}");
+Console.WriteLine("Produto 2");
+Console.Write("\tNome ..: ");
+nomeProd2 = Console.ReadLine();
+Console.Write("\tPreço ..: ");
+float.TryParse(Console.ReadLine(), out precoProd2);
+
+Console.WriteLine($"{nomeProd2} {precoProd2, 10:C2}");
 
 Console.ReadKey();
