bae0cb610795d3486a5a199d6c72a739c3221a98
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-07 08:09:59 +0100

    M01A14 - Exercício 009

--- Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX009/Program.cs
+++ Modulo_01_Estruturas_Basicas/M01A14_Exercícios_C_Sharp/M01A14_Exercicios/M01A14_EX009/Program.cs
@@ -8,12 +8,14 @@ nomeProd = Console.ReadLine();
 Console.Write("\tPreço ..: ");
 float.TryParse(Console.ReadLine(), out precoProd);
 
+Console.WriteLine();
 //Segundo produto
 Console.WriteLine("Produto 2");
 Console.Write("\tNome ..: ");
 nomeProd2 = Console.ReadLine();
 Console.Write("\tPreço ..: ");
 float.TryParse(Console.ReadLine(), out precoProd2);
+
 
 Console.WriteLine($"{nomeProd} {precoProd,10:C2}");
 Console.WriteLine($"{nomeProd2} {precoProd2, 10:C2}");
