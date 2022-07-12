58fb950d1851064d5241717f6b1aad059908fbd0
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-12 14:35:05 +0100

    M02A02 – Operadores Aritméticos

--- Modulo_02_Operadores/M02A02_Operadores_Aritmeticos/M0202/M0202/Program.cs
+++ Modulo_02_Operadores/M02A02_Operadores_Aritmeticos/M0202/M0202/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Errado  " + 3+2 /2);
+﻿Console.WriteLine("Errado: " + 3+2 /2);
 
 Console.WriteLine();
-Console.WriteLine("Correto" + (3+2)/2f);
+Console.WriteLine("Correto: " + (3+2)/2f);
 
 Console.ReadKey();
