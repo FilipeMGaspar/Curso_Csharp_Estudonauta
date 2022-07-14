ae0df443f4728f96ec053f5aba798e61a3636650
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-14 08:25:04 +0100

    M02A03 – Prática dos Operadores Aritméticos

--- Modulo_02_Operadores/M02A03_Pratica_dos_Operadores_Aritmeticos/M02A03/M02A03/Program.cs
+++ Modulo_02_Operadores/M02A03_Pratica_dos_Operadores_Aritmeticos/M02A03/M02A03/Program.cs
@@ -5,7 +5,8 @@ int op2 = 2;
 
 Console.WriteLine($"Calculando +{op1} = {+ op1}"); //operador de identidade
 Console.WriteLine($"Calculando -{op1} = {- op1}"); //operador de negação aritmética
-Console.WriteLine($"Calculando {op1} + {op2} = {op1 + op2}"); // Soma operador binário
+Console.WriteLine($"Calculando {op1} + {op2} = {op1 + op2}"); // Operador de adição
+Console.WriteLine($"Calculando {op1} - {op2} = {op1 - op2}"); // Operador de subtração
 Console.WriteLine($"Calculando {op1} x {op2} = {op1 * op2}"); // operador de multiplicação
 Console.WriteLine($"Calculando {op1} ÷ {op2} = {op1 / op2} ** ERRO ** "); // operadior de divisão inteira
 Console.WriteLine($"Calculando o resto da divisão de {op1} por {op2} = {op1 % op2}"); // resto da divisão inteira
