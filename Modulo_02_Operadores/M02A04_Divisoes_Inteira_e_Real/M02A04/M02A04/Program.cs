02562859cd4bdbf7f60d3a05743e3e5b05b9b7a6
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-14 08:55:30 +0100

    M02A04 – Divisões Inteira e Real

--- Modulo_02_Operadores/M02A04_Divisoes_Inteira_e_Real/M02A04/M02A04/Program.cs
+++ Modulo_02_Operadores/M02A04_Divisoes_Inteira_e_Real/M02A04/M02A04/Program.cs
@@ -2,7 +2,10 @@
 int op2 = 2;
 
 int divInt = op1 / op2;
-float divReal = op1 / op2;
+
+float divReal = (float)op1 / op2;
 
 Console.WriteLine($"Divisão inteira {op1} ÷ {op2} = {divInt}");
 Console.WriteLine($"Divisão real {op1} ÷ {op2} = {divReal}");
+
+Console.ReadKey();
