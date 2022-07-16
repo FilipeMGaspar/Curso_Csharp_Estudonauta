ae149d35f99c87d8bd9a9a1a37a55a971f2c289b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-16 09:47:43 +0100

    M02A05 – Precedência dos Operadores Aritméticos

--- Modulo_02_Operadores/M02A05_Precedencia_dos_Operadores_Aritmeticos/M02A05/M02A05/Program.cs
+++ Modulo_02_Operadores/M02A05_Precedencia_dos_Operadores_Aritmeticos/M02A05/M02A05/Program.cs
@@ -1,7 +1,7 @@
 ﻿float nota1 = 3f;
 float nota2 = 4f;
 
-float media = nota1 + nota2 / 2;
+float media = (nota1 + nota2) / 2;
 
 Console.WriteLine($"As notas registadas foram {nota1:F1} pontos e {nota2:F1} pontos");
 Console.WriteLine($"A média do aluno foi {media:F1} pontos");
