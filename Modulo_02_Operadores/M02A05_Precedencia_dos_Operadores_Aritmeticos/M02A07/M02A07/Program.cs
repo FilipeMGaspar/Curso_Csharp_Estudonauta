985d5ed0bd2ffd0e2d5a45166e7d5401c9e8988b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-18 08:13:02 +0100

    M02A07 – Operadores Relacionais

--- Modulo_02_Operadores/M02A05_Precedencia_dos_Operadores_Aritmeticos/M02A07/M02A07/Program.cs
+++ Modulo_02_Operadores/M02A05_Precedencia_dos_Operadores_Aritmeticos/M02A07/M02A07/Program.cs
@@ -3,5 +3,9 @@ int n2 = 6;
 
 Console.WriteLine($"{n1} == {n2} ? {n1 == n2}");
 Console.WriteLine($"{n1} != {n2} ? {n1 != n2}");
+Console.WriteLine($"{n1} > {n2} ? {n1 > n2}");
+Console.WriteLine($"{n1} < {n2} ? {n1 < n2}");
+Console.WriteLine($"{n1} >= {n2} ? {n1 >= n2}");
+Console.WriteLine($"{n1} <= {n2} ? {n1 <= n2}");
 
 Console.ReadKey();
