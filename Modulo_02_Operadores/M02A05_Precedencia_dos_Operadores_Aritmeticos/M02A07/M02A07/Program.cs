3bebab748aaf763e36ff50e25a240feb179e3585
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-07-18 08:21:00 +0100

    M02A07 – Operadores Relacionais

--- Modulo_02_Operadores/M02A05_Precedencia_dos_Operadores_Aritmeticos/M02A07/M02A07/Program.cs
+++ Modulo_02_Operadores/M02A05_Precedencia_dos_Operadores_Aritmeticos/M02A07/M02A07/Program.cs
@@ -1,16 +1,20 @@
-﻿int n1 = 0;
+﻿// Entrada de dados
+int n1 = 0;
 int n2 = 0;
 
-Console.Write("Vaolor 1: ");
+Console.Write("Valor 1: ");
 int.TryParse(Console.ReadLine(), out n1);
 Console.Write("Valor 2: ");
 int.TryParse(Console.ReadLine(), out n2);
 
+//Saida de dados
+Console.WriteLine();
 Console.WriteLine($"{n1} == {n2} ? {n1 == n2}");
 Console.WriteLine($"{n1} != {n2} ? {n1 != n2}");
 Console.WriteLine($"{n1} > {n2} ? {n1 > n2}");
 Console.WriteLine($"{n1} < {n2} ? {n1 < n2}");
 Console.WriteLine($"{n1} >= {n2} ? {n1 >= n2}");
 Console.WriteLine($"{n1} <= {n2} ? {n1 <= n2}");
+Console.WriteLine($"{n1} é inteiro {n1 is int}");
 
 Console.ReadKey();
